d2fbd94c30f82119cb2ea8c3bcf825ffefc814a7
Pmichaud69 <micp1402@usherbrooke.ca>
2023-01-25 20:27:50 -0500

    Multiples changes
    - Fixed CMake
    - Added .yaml config for controller and both XL430 motors
    - Added launchfile for controller node
    - Added publisher to motor

--- arm_us/src/master_node.cpp
+++ arm_us/src/master_node.cpp
@@ -1,13 +1,18 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Joy.h"
+#include "sensor_msgs/JointState.h"
 
 void init();
 void loop();
 void sub_input_callback(const sensor_msgs::Joy::ConstPtr &data);
 void update_btn1();
+void sendCmdMotor();
 
 bool btn1 = false;
+float axe1 = 0.0;
+
 ros::Subscriber sub_input;
+ros::Publisher pub_motor;
 
 int main(int argc, char* argv[])
 {
@@ -22,8 +27,8 @@ int main(int argc, char* argv[])
 void init()
 {
     ros::NodeHandle n;
-
     sub_input = n.subscribe("joy", 1, sub_input_callback);
+    pub_motor = n.advertise<sensor_msgs::JointState>("desired_joint_states", 10);
 }
 
 void loop()
@@ -33,6 +38,7 @@ void loop()
     while(ros::ok())
     {
         update_btn1();
+        sendCmdMotor();
     
         ros::spinOnce();
         loop_rate.sleep();
@@ -42,6 +48,9 @@ void loop()
 void sub_input_callback(const sensor_msgs::Joy::ConstPtr &data)
 {
     btn1 = data->buttons[0];
+    axe1 = data->axes[2];
+
+    axe1 = abs((axe1-1)/2 * 5);
 }
 
 void update_btn1()
@@ -50,4 +59,14 @@ void update_btn1()
     {
         ROS_WARN("Button 1 pressed");
     }
+}
+
+void sendCmdMotor()
+{
+    sensor_msgs::JointState msg;
+
+    msg.name = {"motor1", "motor2"};
+    msg.velocity = {axe1, axe1};
+
+    pub_motor.publish(msg);
 }
