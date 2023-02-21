80b80c16a38a5402a270731db3096bdb383773d3
Mikael <stam1001@usherbrooke.ca>
2023-02-20 22:44:58 -0500

    Gui with velocity controls and current motor positions

--- arm_us/src/master_node.cpp
+++ arm_us/src/master_node.cpp
@@ -3,38 +3,39 @@
 #include "sensor_msgs/JointState.h"
 #include "arm_us/MotorControl.h"
 
-#define MAX_VEL 4.8 //Linked to config/yaml file (global_max_vel )
-#define MIN_DIFF -3 //-8.75
-#define MAX_DIFF 3.00
+#define MAX_VEL     4.8     //Linked to config/yaml file (global_max_vel )
+#define MIN_DIFF    -3.00     //-8.75
+#define MAX_DIFF    3.00
 
 #define ROS_RATE 50
 
 void init();
 void loop();
-void subInputCallback(const sensor_msgs::Joy::ConstPtr &data);
-void subGuiCallback(const arm_us::MotorControl::ConstPtr &data);
-void sendCmdMotor(bool sendZeros = 0);
+
+void sub_input_callback(const sensor_msgs::Joy::ConstPtr &data);
+void sub_gui_callback(const arm_us::MotorControl::ConstPtr &data);
 void sub_joint_states_callback(const sensor_msgs::JointState::ConstPtr &data);
+void send_cmd_motor(bool sendZeros = 0);
 
 float joystickLeftVert = 0.0f;
 float joystickLeftSide = 0.0f;
 float joystickRightVert = 0.0f;
 float joystickRightSide = 0.0f;
+
 ros::Subscriber sub_input;
 ros::Subscriber sub_gui;
+ros::Subscriber sub_joint_states;
+ros::Publisher pub_motor;
 
 float position_difference = 0.0f;
+
 struct MotorVelocity
-ros::Subscriber sub_joint_states;
 {
     float m1 = 0.0f;
     float m2 = 0.0f;
     float m3 = 0.0f;
     float m4 = 0.0f;
 } motorVelocity;
-ros::Publisher pub_motor;
-
-const float maxMotorSpeed = 4.8f;
 
 arm_us::MotorControl motors[2];
 
@@ -57,8 +58,9 @@ void init()
     ros::NodeHandle n;
     sub_input =         n.subscribe("joy", 1, sub_input_callback);
     sub_joint_states =  n.subscribe("joint_states", 1, sub_joint_states_callback);
+    sub_gui =           n.subscribe("gui_arm_us_chatter", 1, sub_gui_callback);
     pub_motor =         n.advertise<sensor_msgs::JointState>("desired_joint_states", 10);
-    sub_gui = n.subscribe("gui_arm_us_chatter", 1, subGuiCallback);
+    
 }
 
 void loop()
@@ -67,17 +69,17 @@ void loop()
 
     while (ros::ok())
     {
-        sendCmdMotor();
+        send_cmd_motor();
         ros::spinOnce();
         loop_rate.sleep();
     }
 
-    sendCmdMotor(true);
+    send_cmd_motor(true);
 
     ros::shutdown();
 }
 
-void subInputCallback(const sensor_msgs::Joy::ConstPtr &data)
+void sub_input_callback(const sensor_msgs::Joy::ConstPtr &data)
 {   
     joystickLeftVert =  data->axes[1] * MAX_VEL;
     joystickLeftSide =  data->axes[0] * MAX_VEL;
@@ -86,12 +88,40 @@ void subInputCallback(const sensor_msgs::Joy::ConstPtr &data)
 
 }
 
-void subGuiCallback(const arm_us::MotorControl::ConstPtr &data)
-void sub_joint_states_callback(const sensor_msgs::JointState::ConstPtr &data)
+void sub_gui_callback(const arm_us::MotorControl::ConstPtr &data)
 {
-    motors[data->motor_id].velocity = data->velocity;
+    int id = data->motor_id;
+    motors[id].enabled = !motors[id].enabled;
+    motors[id].velocity = data->velocity;
 }
 
+
+void sub_joint_states_callback(const sensor_msgs::JointState::ConstPtr &data)
+{
+    position_difference = data->position[0] - data->position[1];
+}
+
+void send_cmd_motor(bool sendZeros)
+{
+    sensor_msgs::JointState msg;
+    msg.name = { "motor1", "motor2" };
+
+    if (sendZeros)
+    {
+        msg.velocity = { 0.0, 0.0 };
+        pub_motor.publish(msg);
+        ROS_WARN("Zeros sent");
+    }
+
+    motorVelocity.m1 = (joystickLeftVert + joystickRightSide)/2;
+    motorVelocity.m2 = (joystickLeftVert - joystickRightSide)/2;
+
+    if (motorVelocity.m1 > 0 && position_difference < MIN_DIFF)
+    {
+        ROS_WARN("At Limit, go the other way");
+        motorVelocity.m1 = 0.0;
+        motorVelocity.m2 = 0.0;
+    }
 
     if (motorVelocity.m1 < 0 && position_difference > MAX_DIFF)
     {
@@ -103,7 +133,6 @@ void sub_joint_states_callback(const sensor_msgs::JointState::ConstPtr &data)
     msg.velocity = { motorVelocity.m1, motorVelocity.m2 };
     // ROS_WARN("%f | %f", msg.velocity[0], msg.velocity[1]);
 
-    //ROS_WARN("Motor 1 : %f, Motor 2 : %f", motors[0].velocity, motors[1].velocity);
-
     pub_motor.publish(msg);
+
 }
--- gui_arm_us/src/gui_arm_us/gui_arm_us_widget.py
+++ gui_arm_us/src/gui_arm_us/gui_arm_us_widget.py
@@ -14,6 +14,7 @@ from PyQt5.QtWidgets import QShortcut, QSlider, QLCDNumber, QLabel, QPushButton,
 import rosservice
 
 from arm_us.msg import MotorControl
+from sensor_msgs.msg import JointState
 
 
 class GuiArmUsWidget(QtWidgets.QWidget):
@@ -24,6 +25,11 @@ class GuiArmUsWidget(QtWidgets.QWidget):
 
     motor_statuses.append(MotorControl(0, False, 0.0))
     motor_statuses.append(MotorControl(1, False, 0.0))
+
+    motor_velocities = []
+
+    motor_velocities.append(max_velocity)
+    motor_velocities.append(max_velocity)
 
     def __init__(self):
     
@@ -36,11 +42,25 @@ class GuiArmUsWidget(QtWidgets.QWidget):
         self.launch: roslaunch.ROSLaunch = roslaunch.scriptapi.ROSLaunch()
         self.launch.start()
 
-        self.pub_gui_arm_us_cmd = rospy.Publisher('gui_arm_us_chatter', MotorControl)
+        self.sub_motor_positions = rospy.Subscriber("joint_states", JointState, self.motor_positions_callback)
+
+        self.pub_gui_arm_us_cmd = rospy.Publisher('gui_arm_us_chatter', MotorControl, queue_size=1)
         self.rate = rospy.Rate(10) # 10Hz
 
         self.motor1_button.released.connect(self.motor1_button_callback)
         self.motor2_button.released.connect(self.motor2_button_callback)
+
+        self.motor1_velocity.setValue(self.max_velocity)
+        self.motor2_velocity.setValue(self.max_velocity)
+
+
+        self.motor1_velocity.setRange(0, self.max_velocity)
+        self.motor2_velocity.setRange(0, self.max_velocity)
+
+        self.motor1_velocity.valueChanged.connect(self.motor1_velocity_callback)
+        self.motor2_velocity.valueChanged.connect(self.motor2_velocity_callback)
+
+
 
     def motor1_button_callback(self):
         if self.motor_statuses[0].enabled:
@@ -48,10 +68,11 @@ class GuiArmUsWidget(QtWidgets.QWidget):
             self.motor_statuses[0].velocity = 0
         else:
             self.motor_statuses[0].enabled = True
-            self.motor_statuses[0].velocity = self.max_velocity
+            self.motor_statuses[0].velocity = self.motor_velocities[0]
 
-        rospy.loginfo("Motor 1 : %s, velocity : %f", self.motor_statuses[0].enabled, self.motor_statuses[0].velocity)
+        rospy.loginfo("Motor 1 : %s", self.motor_statuses[0].enabled)
         self.pub_gui_arm_us_cmd.publish(self.motor_statuses[0])
+
 
 
     def motor2_button_callback(self):
@@ -60,8 +81,26 @@ class GuiArmUsWidget(QtWidgets.QWidget):
             self.motor_statuses[1].velocity = 0
         else:
             self.motor_statuses[1].enabled = True
-            self.motor_statuses[1].velocity = self.max_velocity
+            self.motor_statuses[1].velocity = self.motor_velocities[1]
 
-        rospy.loginfo("Motor 1 : %s, velocity : %f", self.motor_statuses[1].enabled, self.motor_statuses[1].velocity)
+        rospy.loginfo("Motor 2 : %s", self.motor_statuses[1].enabled)
         self.pub_gui_arm_us_cmd.publish(self.motor_statuses[1])
 
+
+
+    def motor1_velocity_callback(self, velocity):
+        self.motor_velocities[0] = velocity
+        rospy.loginfo("Motor 1 velocity : %f", self.motor_velocities[0])
+
+
+
+    def motor2_velocity_callback(self, velocity):
+        self.motor_velocities[1] = velocity
+        rospy.loginfo("Motor 1 velocity : %f", self.motor_velocities[1])
+
+
+    
+    def motor_positions_callback(self, positions):
+        self.motor1_position.setNum(positions[0].position)
+        self.motor2_position.setNum(positions[1].position)
+        rospy.loginfo("Motor 1 position : %f, motor 2 position : %f", positions[0].position, positions[1].position)
