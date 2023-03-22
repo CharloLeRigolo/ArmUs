64da513a8f6f2ade2335b51a4635bc30022ed470
Mikael <stam1001@usherbrooke.ca>
2023-03-21 21:20:00 -0400

    Implementation of interface for simulation and real modes

--- arm_us/src/master_node.cpp
+++ arm_us/src/master_node.cpp
@@ -6,7 +6,7 @@
 #include "arm_us/GuiInfo.h"
 #include "arm_us/GraphInfo.h"
 
-
+#include <memory>
 
 enum class ControlMode { Real = 0, Simulation = 1 };
 
@@ -80,7 +80,6 @@ struct Controller
             ROS_WARN("Buttons (1, 2, 3, 4) = %d, %d, %d, %d", Buttons.Button1, Buttons.Button2, Buttons.Button3, Buttons.Button4);
             ROS_WARN("Bumpers (L, R) = %d, %d", Bumpers.Left, Bumpers.Right);
             ROS_WARN("Triggers (L, R) = %d, %d", Triggers.Left, Triggers.Right);
-            
         }
     }
 
@@ -107,10 +106,12 @@ struct Vector5f
         m5 = f5;
     }
 
-    std::vector<float> get()
+    std::vector<double> get()
     {
         return { m1, m2, m3, m4, m5 };
     }
+
+    float get(){}
 
     float m1, m2, m3, m4, m5;
 };
@@ -126,7 +127,7 @@ struct Vector5b
         m5 = b5;   
     }
 
-    std::vector<bool> get()
+    std::vector<uint8_t> get()
     {
         return { m1, m2, m3, m4, m5 };
     }
@@ -134,8 +135,42 @@ struct Vector5b
     bool m1, m2, m3, m4, m5;
 };
 
-struct ArmUsInfo
+struct Vector3f
 {
+    void set(float xx, float yy, float zz)
+    {
+        x = xx;
+        y = yy;
+        z = zz;
+    }
+
+    float x, y, z;
+};
+
+class ArmUsInfo
+{
+public:
+
+    virtual void calculate_motor_velocities() = 0;
+
+    void calculate_joint_angles()
+    {
+        JointAngles.m1 = convert_motor_pos_to_deg(MotorPositions.m1 + MotorPositions.m2);
+        JointAngles.m2 = convert_motor_pos_to_deg(MotorPositions.m1 - MotorPositions.m2);
+        if (verbose)
+        {
+            ROS_WARN("j1 : %f, j2 : %f", MotorPositions.m1, MotorPositions.m2);
+        }
+    }
+
+    float convert_motor_pos_to_deg(float current_pos, float min_input = 0, float max_input = 4095, float min_val = 0, float max_val = 360)
+    {
+        return ((current_pos / (max_input - min_input)) * (max_val - min_val)) + min_val;
+    }
+
+    float JointCommand;
+    Vector3f CartesianCommand;
+
     Vector5f MotorPositions;
     Vector5f MotorVelocities;
     Vector5f JointAngles;
@@ -143,11 +178,37 @@ struct ArmUsInfo
     Vector5b MotorConnections;
     Vector5b MotorLimits;
 
-    MovementMode MoveMode = MovementMode::Cartesian;
+    MovementMode MoveMode = MovementMode::Joint;
     
     int JointControlled = 1;
 
     float PositionDifference = 0.0f;
+};
+
+class ArmUsInfoSimul : public ArmUsInfo
+{
+    void calculate_motor_velocities()
+    {
+        if (MoveMode == MovementMode::Joint)
+        {
+            MotorVelocities;
+        }
+        else if (MoveMode == MovementMode::Cartesian)
+        {
+
+        }
+    }
+};
+
+class ArmUsInfoReal : public ArmUsInfo
+{
+    void calculate_motor_velocities()
+    {
+        switch(JointControlled)
+        {
+
+        }
+    }
 };
 
 /******************** ArmUs ********************/
@@ -156,6 +217,18 @@ class ArmUs
 {
 public:
     
+    ArmUs(ControlMode controlMode) : m_controlMode(controlMode)
+    {
+        if (controlMode == ControlMode::Real)
+        {
+            m_arm_us_info = std::make_unique<ArmUsInfoReal>();
+        }
+        else if (controlMode == ControlMode::Simulation)
+        {
+            m_arm_us_info = std::make_unique<ArmUsInfoSimul>();
+        }
+    }
+
     void Run()
     {
         ros::Rate loop_rate(ROS_RATE);
@@ -172,9 +245,12 @@ public:
     void Initalize()
     {
         m_sub_input =         m_nh.subscribe("joy", 1, &ArmUs::subControllerCallback, this);
-        m_sub_joint_states =  m_nh.subscribe("joint_states", 1, &ArmUs::sub_joint_states_callback, this);
         m_sub_gui =           m_nh.subscribe("gui_arm_us_chatter", 1, &ArmUs::sub_gui_callback, this);
-
+        if (m_controlMode == ControlMode::Real)
+        {
+            m_sub_joint_states =  m_nh.subscribe("joint_states", 1, &ArmUs::sub_joint_states_callback, this);
+        }
+       
         m_pub_motor =         m_nh.advertise<sensor_msgs::JointState>("desired_joint_states", 10);
         m_pub_gui =           m_nh.advertise<arm_us::GuiInfo>("gui_info", 10);
         m_pub_3d_graph =      m_nh.advertise<arm_us::GraphInfo>("graph_info", 10);
@@ -186,36 +262,90 @@ private:
 
     void subControllerCallback(const sensor_msgs::Joy::ConstPtr &data)
     {
+        
         m_controller.JoyLeft.set(data->axes[LEFT_JOY_VERT], data->axes[LEFT_JOY_HORI]);
         m_controller.JoyRight.set(data->axes[RIGHT_JOY_VERT], data->axes[RIGHT_JOY_HORI]);
+
+        /*
         m_controller.Pad.set(data->axes[PAD_VERT], data->axes[PAD_HORI]);
         m_controller.Buttons.set(data->buttons[BUTTON_1], data->buttons[BUTTON_2], data->buttons[BUTTON_3], data->buttons[BUTTON_4]);
         m_controller.Bumpers.set(data->buttons[LEFT_BUMP], data->buttons[RIGHT_BUMP]);
         m_controller.Triggers.set(data->buttons[LEFT_TRIG], data->buttons[RIGHT_TRIG]);
-
         m_controller.DisplayControllerInputs();
+        */
+
+        // Switch modes (cartesian and joint) with controller
+        if (data->buttons[BUTTON_3] == 1 && m_controller.Buttons.Button3 == 0)
+        {
+            if (m_arm_us_info->MoveMode == MovementMode::Cartesian)
+            {
+                m_arm_us_info->MoveMode = MovementMode::Joint;
+                ROS_WARN("Joint");
+            }
+            else 
+            {
+                m_arm_us_info->MoveMode = MovementMode::Cartesian;
+                ROS_WARN("Cartesian");
+            }
+        }
+        
+        /********** Joint **********/
+        if (m_arm_us_info->MoveMode == MovementMode::Joint)
+        {
+            // Change joint controlled with controller
+            if (data->buttons[BUTTON_4] == 1 && m_controller.Buttons.Button4 == 0)
+            {
+                m_arm_us_info->JointControlled++;
+                if (m_arm_us_info->JointControlled > 5)
+                {
+                    m_arm_us_info->JointControlled = 1;
+                }
+                ROS_WARN("Joint controlled : %d", m_arm_us_info->JointControlled);
+            }
+
+            if (data->buttons[BUTTON_2] == 1 && m_controller.Buttons.Button2 == 0)
+            {
+                m_arm_us_info->JointControlled--;
+                if (m_arm_us_info->JointControlled < 1)
+                {
+                    m_arm_us_info->JointControlled = 5;
+                }
+                ROS_WARN("Joint controlled : %d", m_arm_us_info->JointControlled);
+            }
+
+            // Set speed of joint
+            m_arm_us_info->JointCommand = m_controller.JoyLeft.Vertical * MAX_VEL;
+
+        }
+        /********** Cartesian **********/
+        else if (m_arm_us_info->MoveMode == MovementMode::Cartesian)
+        {
+
+        }
+
+        m_controller.Buttons.set(data->buttons[BUTTON_1], data->buttons[BUTTON_2], data->buttons[BUTTON_3], data->buttons[BUTTON_4]);
     }
 
     void sub_gui_callback(const arm_us::GuiFeedback::ConstPtr &data)
     {
         if (data->joint)
         {
-            m_arm_us_info.MoveMode = MovementMode::Joint;
+            m_arm_us_info->MoveMode = MovementMode::Joint;
         }
         else if (data->cartesian)
         {
-            m_arm_us_info.MoveMode = MovementMode::Cartesian;
+            m_arm_us_info->MoveMode = MovementMode::Cartesian;
         }
 
-        m_arm_us_info.JointControlled = data->joint_controlled;
+        m_arm_us_info->JointControlled = data->joint_controlled;
     }
 
     void sub_joint_states_callback(const sensor_msgs::JointState::ConstPtr &data)
     {
-        m_arm_us_info.MotorPositions.set(data->position[0], data->position[1], data->position[2], data->position[3], data->position[4]);
-        m_arm_us_info.MotorVelocities.set(data->velocity[0], data->velocity[1], data->velocity[2], data->velocity[3], data->velocity[4]);
+        m_arm_us_info->MotorPositions.set(data->position[0], data->position[1], data->position[2], data->position[3], data->position[4]);
+        m_arm_us_info->MotorVelocities.set(data->velocity[0], data->velocity[1], data->velocity[2], data->velocity[3], data->velocity[4]);
 
-        m_arm_us_info.PositionDifference = data->position[0] - data->position[1];
+        m_arm_us_info->PositionDifference = data->position[0] - data->position[1];
     }
 
     void setParams()
@@ -229,6 +359,7 @@ private:
         m_nh.getParam("/master_node/pad_hori", PAD_HORI);
         m_nh.getParam("/master_node/pad_vert", PAD_VERT);
 
+
         m_nh.getParam("/master_node/button_1", BUTTON_1);
         m_nh.getParam("/master_node/button_2", BUTTON_2);
         m_nh.getParam("/master_node/button_3", BUTTON_3);
@@ -239,6 +370,44 @@ private:
 
         m_nh.getParam("/master_node/left_trig", LEFT_TRIG);
         m_nh.getParam("/master_node/right_trig", RIGHT_TRIG);
+    }
+
+    void send_cmd_motor()
+    {
+        sensor_msgs::JointState msg;
+        msg.name = { "motor1", "motor2" , "motor3", "motor4", "motor5" };
+        msg.velocity = m_arm_us_info->MotorVelocities.get();
+        m_pub_motor.publish(msg);
+    }
+
+    void send_cmd_motor_stop()
+    {
+        sensor_msgs::JointState msg;
+        msg.name = { "motor1", "motor2", "motor3", "motor4", "motor5" };
+        msg.velocity = { 0.0, 0.0, 0.0, 0.0, 0.0 };
+        m_pub_motor.publish(msg);
+        if (verbose)
+        {
+            ROS_WARN("All motors stopped");
+        }
+    }
+
+    void send_gui_info()
+    {
+        arm_us::GuiInfo msg;
+
+        msg.position = m_arm_us_info->MotorPositions.get();
+        msg.velocity = m_arm_us_info->MotorVelocities.get();
+        msg.connected = m_arm_us_info->MotorConnections.get();
+        msg.limit_reached = m_arm_us_info->MotorLimits.get();
+        m_pub_gui.publish(msg);
+    }
+
+    void send_3d_graph_info()
+    {
+        arm_us::GraphInfo msg;
+        msg.angle = m_arm_us_info->JointAngles.get();
+        m_pub_3d_graph.publish(msg);
     }
 
     ros::NodeHandle m_nh;
@@ -253,7 +422,9 @@ private:
 
     Controller m_controller;
 
-    ArmUsInfo m_arm_us_info;
+    std::unique_ptr<ArmUsInfo> m_arm_us_info;
+
+    ControlMode m_controlMode;
 
     /********** Constantes **********/
 
@@ -280,11 +451,13 @@ private:
     const int ROS_RATE = 50;
 };
 
+/******************** Main ********************/
+
 int main(int argc, char* argv[])
 {
     ros::init(argc, argv, "master_node");
 
-    ArmUs arm_us;
+    ArmUs arm_us(controlMode);
 
     arm_us.Initalize();
     arm_us.Run();
@@ -293,26 +466,6 @@ int main(int argc, char* argv[])
 }
 
 /*
-void send_cmd_motor();
-void send_cmd_motor_stop();
-void send_gui_info();
-void send_3d_graph_info();
-
-
-
-
-
-void calculate_motor_velocities();
-void calculate_joint_angles();
-
-float convert_motor_pos_to_deg(float current_pos, float min_input = 0, float max_input = 4095, float min_val = 0, float max_val = 360);
-
-
-
-
-
-
-
 
 void loop()
 {
@@ -354,46 +507,6 @@ void loop()
 
 
 
-void send_cmd_motor()
-{
-    sensor_msgs::JointState msg;
-    msg.name = { "motor1", "motor2" , "motor3", "motor4", "motor5" };
-    msg.velocity = { motorVelocitiesCmd.m1, motorVelocitiesCmd.m2, motorVelocitiesCmd.m3, motorVelocitiesCmd.m4, motorVelocitiesCmd.m5 };
-    pub_motor.publish(msg);
-}
-
-void send_cmd_motor_stop()
-{
-    sensor_msgs::JointState msg;
-    msg.name = { "motor1", "motor2", "motor3", "motor4", "motor5" };
-    msg.velocity = { 0.0, 0.0, 0.0, 0.0, 0.0 };
-    pub_motor.publish(msg);
-    ROS_WARN("All motors stopped");
-}
-
-void send_gui_info()
-{
-    arm_us::GuiInfo msg;
-
-    msg.position = { g_armInfo.motorPositions[0], g_armInfo.motorPositions[1], g_armInfo.motorPositions[2], g_armInfo.motorPositions[3], g_armInfo.motorPositions[4] };
-    msg.velocity = { g_armInfo.motorVelocities[0], g_armInfo.motorVelocities[1], g_armInfo.motorVelocities[2], g_armInfo.motorVelocities[3], g_armInfo.motorVelocities[4] };
-    msg.connected = { g_armInfo.motorConnected[0], g_armInfo.motorConnected[1], g_armInfo.motorConnected[2], g_armInfo.motorConnected[3], g_armInfo.motorConnected[4] };
-    msg.limit_reached = { g_armInfo.motorLimitReached[0], g_armInfo.motorLimitReached[1], g_armInfo.motorLimitReached[2], g_armInfo.motorLimitReached[3], g_armInfo.motorLimitReached[4] };
-    
-    pub_gui.publish(msg);
-}
-
-void send_3d_graph_info()
-{
-    arm_us::GraphInfo msg;
-    msg.angle = { jointAngles.j1, jointAngles.j2, jointAngles.j3, jointAngles.j4, jointAngles.j5 };
-    pub_3d_graph.publish(msg);
-}
-
-float convert_motor_pos_to_deg(float current_pos, float min_input, float max_input, float min_val, float max_val)
-{
-    return ((current_pos / (max_input - min_input)) * (max_val - min_val)) + min_val;
-}
 
 void calculate_motor_velocities()
 {
@@ -429,10 +542,4 @@ void calculate_motor_velocities()
     }
 }
 
-void calculate_joint_angles()
-{
-    jointAngles.j1 = convert_motor_pos_to_deg(g_armInfo.motorPositions[0] + g_armInfo.motorPositions[1]);
-    jointAngles.j2 = convert_motor_pos_to_deg(g_armInfo.motorPositions[0] - g_armInfo.motorPositions[1]);
-    // ROS_WARN("j1 : %f, j2 : %f", jointAngles.j1, jointAngles.j2);
-}
 */
