1852134fc1f53638098a333e1457ebe3729f8665
Pmichaud69 <93682089+Pmichaud69@users.noreply.github.com>
2023-04-07 09:28:57 -0400

    Modified code for random motor order

--- arm_us/src/motor_translator.cpp
+++ arm_us/src/motor_translator.cpp
@@ -5,8 +5,8 @@
 #define NB_JOINT 5
 const std::string NODE_NAME = "motor_translator";
 
-void command_callback(const sensor_msgs::JointStateConstPtr &msg);
-void state_callback(const sensor_msgs::JointStateConstPtr &msg);
+void commandCallback(const sensor_msgs::JointStateConstPtr &msg);
+void stateCallback(const sensor_msgs::JointStateConstPtr &msg);
 
 ros::Publisher pub_command;
 ros::Publisher pub_angles;
@@ -20,14 +20,15 @@ std::unordered_map<std::string, int> motor_map = {
     {"motor5", 4}
 };
 
+short motor_order[5] = {0, 1, 2, 3, 4};
 
 int main(int argc, char *argv[])
 {
     ros::init(argc, argv, NODE_NAME);
     ros::NodeHandle n;
 
-    ros::Subscriber sub_command = n.subscribe("raw_desired_joint_states", 1, command_callback);
-    ros::Subscriber sub_state = n.subscribe("joint_states", 1, state_callback);
+    ros::Subscriber sub_command = n.subscribe("raw_desired_joint_states", 1, commandCallback);
+    ros::Subscriber sub_state = n.subscribe("joint_states", 1, stateCallback);
     pub_command = n.advertise<sensor_msgs::JointState>("motor_topic", 1);
     pub_angles = n.advertise<sensor_msgs::JointState>("angles_joint_state", 1);
 
@@ -37,7 +38,7 @@ int main(int argc, char *argv[])
     }
 }
 
-void command_callback(const sensor_msgs::JointStateConstPtr &msg)
+void commandCallback(const sensor_msgs::JointStateConstPtr &msg)
 {
     sensor_msgs::JointState cmd;
 
@@ -70,7 +71,7 @@ void command_callback(const sensor_msgs::JointStateConstPtr &msg)
     pub_command.publish(cmd);
 }
 
-void state_callback(const sensor_msgs::JointStateConstPtr &msg)
+void stateCallback(const sensor_msgs::JointStateConstPtr &msg)
 {
     sensor_msgs::JointState angle_feedback;
 
@@ -79,7 +80,7 @@ void state_callback(const sensor_msgs::JointStateConstPtr &msg)
     double pos_max_angles[NB_JOINT];
     double pos_min_angles[NB_JOINT];
 
-    for (short i = 0, j = 0; i < NB_JOINT; i++)
+    for (short i = 0; i < NB_JOINT; i++)
     {
         ros::param::get("/" + NODE_NAME + "/j" + std::to_string(i + 1) + "/pos_max_angle", pos_max_angles[i]);
         ros::param::get("/" + NODE_NAME + "/j" + std::to_string(i + 1) + "/pos_min_angle", pos_min_angles[i]);
@@ -88,9 +89,16 @@ void state_callback(const sensor_msgs::JointStateConstPtr &msg)
     }
 
     angle_feedback = *msg;
-    for (auto i = 2; i < NB_JOINT; i++)
+    for (auto i = 0, motor_index = 0; i < NB_JOINT; i++)
     {
-        joint_angles[i] = min_angles[i] + ((msg->position[i] - pos_min_angles[i]) * (max_angles[i] - min_angles[i]) / (pos_max_angles[i] - pos_min_angles[i]));
+        motor_index = motor_map[msg->name[i]];
+        joint_angles[motor_index] = min_angles[motor_index] + ((msg->position[motor_index] - pos_min_angles[motor_index]) * (max_angles[motor_index] - min_angles[motor_index]) / (pos_max_angles[motor_index] - pos_min_angles[motor_index]));
+    }
+
+    angle_feedback.name = {"motor1", "motor2", "motor3", "motor4", "motor5"};
+
+    for (auto i=0; i < NB_JOINT; i++)
+    {
         angle_feedback.position[i] = joint_angles[i];
     }
 
